0fc549001cd49a48a25835694b5a3e2ea79c92af
David Shanahan <david.shanahan@hotmail.com>
2021-02-17 16:33:30 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/davidshanahan/clima_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.davidshanahan.clima_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  // had to add the forceAndroidLocationManager property here to make it work on the Android Emulator
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+      forceAndroidLocationManager: true,
+    );
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
