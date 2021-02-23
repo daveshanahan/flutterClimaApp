2fa78117381a57c5b659f9652cf3f63c6412ceba
David Shanahan <david.shanahan@hotmail.com>
2021-02-23 18:54:49 +0000

    created lat and long variables inside State class and assigned the location data from the geoLocator package to those variables so we could use them in the API call. Initialized a new network helper object using the API key. Created a new variable weatherData which will hold the parsed JSON

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'package:clima_app/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima_app/services/location.dart';
+import 'package:clima_app/services/networking.dart';
+import 'package:clima_app/utilities/constants.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,15 +10,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = networkHelper.getData();
   }
 
   @override
