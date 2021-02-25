5bea3b69db1b9eea4d3702853f59a4aaa55fbed4
David Shanahan <david.shanahan@hotmail.com>
2021-02-25 18:15:55 +0000

    added comments to the loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -16,19 +16,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
+    // put the method into init state so it is called as soon as the screen is accessed
     getLocationData();
   }
 
   void getLocationData() async {
+    // initialise a location object and call it's get current location method
     Location location = Location();
     await location.getCurrentLocation();
 
+    // assign the lat and long that come back from the method to the lat and long variables
     latitude = location.latitude;
     longitude = location.longitude;
 
+    // initialise new networkHelper object and pass in the URL
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
+    // store the parsed req.body object that is returned as a variable to use later
     var weatherData = networkHelper.getData();
   }
 
