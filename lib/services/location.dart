d2bd673429d362abcad945eec9a524e288de16c2
David Shanahan <david.shanahan@hotmail.com>
2021-02-25 18:11:45 +0000

    added comments to the location.dart file

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,17 +1,21 @@
 import 'package:geolocator/geolocator.dart';
 
+// create a location class to get the lat and long for a location from the geolocator package
 class Location {
   double latitude;
   double longitude;
 
+  // method that gets current location by getting the current position and specifying the accuracy
   Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low,
         forceAndroidLocationManager: true,
       );
+      // assign the lat and long to the positions that come back from the get current position
       latitude = position.latitude;
       longitude = position.longitude;
+      // if there is an error catch it and print it
     } catch (e) {
       print(e);
     }
